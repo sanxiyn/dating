16e641f44a481d9e6d6fccd0de2014a592ac9026
Seo Sanghyeon <sanxiyn@gmail.com>
2017-07-20 02:31:29 +0900

    Use transliteration

--- romancecompass.py
+++ romancecompass.py
@@ -1,5 +1,6 @@
 from base import Spider
 from base import get, get_url
+from utils import transliterate
 
 class RomanceCompassSpider(Spider):
     name = 'romancecompass'
@@ -9,5 +10,8 @@ class RomanceCompassSpider(Spider):
         _, id = get(response, 'div.user-id::text').strip().split(': ')
         location = get(response, 'div.params div:nth-child(1) div:nth-child(1) span::text')
         city, country = location.split(', ')
+        country = country.strip()
+        city = city.strip()
+        city = transliterate(city)
         url = get_url(response, 'div.picture img::attr(src)')
         yield {'id': id, 'country': country, 'city': city, 'url': url}
