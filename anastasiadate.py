4464c7db51061e98348a7bd5c9426f66526497ee
Seo Sanghyeon <sanxiyn@gmail.com>
2017-07-20 03:34:09 +0900

    Handle unavailable profiles

--- anastasiadate.py
+++ anastasiadate.py
@@ -1,12 +1,16 @@
 from base import Spider
 from base import get, get_element, get_url
 
+not_available = "Therefore, it won't be available for a couple of days."
+
 class AnastasiaDateSpider(Spider):
     name = 'anastasiadate'
     url_format = 'http://www.anastasiadate.com/pages/lady/profile/profilepreview.aspx?LadyId={}'
 
     def parse(self, response):
         if 'profile-is-unavailable' in response.url:
+            return
+        if not_available in response.body:
             return
         _, id = get(response, 'div.lady-id span::text').split(': ')
         location = get_element(response, '.ladyProfile-content td:nth-child(2) div:nth-last-child(2)')
